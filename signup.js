c917d68d8c891daf770022dc67542bab3dac9d49
chinky23-tech <chinky0330@gmail.com>
2025-10-06 22:59:35 +0530

    modern ui

--- signup.js
+++ signup.js
@@ -1,14 +1,22 @@
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-function validateInput() {
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex =
+      /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-  if (!emailRegex.test(email)) {
-    alert("Invalid Email!");
-  } else if (!passwordRegex.test(password)) {
-    alert("password not strong!");
-  } else {
-    alert("All good ✅");
-  }
-}
+    function validateInput(event) {
+      event.preventDefault(); // stop page reload
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      if (!emailRegex.test(email)) {
+        alert("Invalid Email! Please enter a valid one.");
+        return false;
+      } else if (!passwordRegex.test(password)) {
+        alert(
+          "Weak Password ❌\nPassword must have:\n- 1 uppercase letter\n- 1 number\n- 1 special character\n- Minimum 8 characters"
+        );
+        return false;
+      } else {
+        alert("Login successful ✅");
+        return true;
+      }
+    }
